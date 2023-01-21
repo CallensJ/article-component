dc0d186f391d518c1c2267dd2610bca43ed6cbd0
Callensj <callens.johan@protonmail.com>
2023-01-21 10:44:26 +0800

    adding javascript modal function

---
+++ index.js
@@ -0,0 +1,21 @@
+const modal = document.querySelector(".modal");
+const btn = document.querySelector(".btn");
+
+
+
+const openModal = function () {
+    
+    modal.classList.remove("hidden");
+    btn.style.backgroundColor ='#000339';
+
+}
+window.onclick = function (e) {
+    if (e.target === modal) {
+        modal.classList.add("hidden");
+        btn.style.backgroundColor = "#d6d2d2";
+
+    }
+}
+
+
+btn.addEventListener("click",openModal);
